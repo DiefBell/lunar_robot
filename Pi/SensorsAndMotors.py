03d2af4b59309ef3ff182bea842b9a9f2d722d44
MerrickKing <merrickkainking@gmail.com>
2019-02-17 17:19:49 +0000

    Test code
    For motors (which can't be tested yet) and getting the Pi's core temp and printing it.

--- Pi/SensorsAndMotors.py
+++ Pi/SensorsAndMotors.py
@@ -1,6 +1,7 @@
 import os
 import time
 from adafruit_motorkit import MotorKit
+from adadfruit_motor import stepper
 
 motors_a = MotorKit()
 motors_b = MotorKit()
@@ -8,7 +9,9 @@ motors_b = MotorKit()
 def measure_temp():
     return os.popen("vcgencmd measure_temp").readline()
 
+motors_a.motor1.throttle = 0.2
 while True:
     print("1")
     print(measure_temp())
+    motors_a.stepper2.onestep(style=stepper.MICROSTEP)
     time.sleep(1)
