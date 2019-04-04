16d437a76cf059fbfeab547ec376f15848590eaf
MerrickKing <merrickkainking@gmail.com>
2019-04-04 20:54:20 +0100

    Added PyCharm project config files, as well as a bit more  HTML :)

--- Pi/Main.py
+++ Pi/Main.py
@@ -1,3 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    t = datetime.now()
+    return render_template('index.html', time=t)
+
+
+if __name__ == "__main__":
+    app.run()
