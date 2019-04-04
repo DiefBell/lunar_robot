095c8d09a306d5a583d0c18e7b18ecf5a4d68e70
MerrickKing <merrickkainking@gmail.com>
2019-04-04 22:05:59 +0100

    Added favicon, downloaded JQuery (it's still not working in an external file...)

--- Pi/Main.py
+++ Pi/Main.py
@@ -1,8 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 
 app = Flask(__name__)
 
+InputMap =\
+{
+    "forward" : "38",
+    "back" : "40",
+    "left" : "37",
+    "right" : "39"
+}
+UserInput =\
+{
+    "37" : False,  # left
+    "38" : False,  # fwd
+    "39" : False,  # right
+    "40" : False  # back
+}
 
 @app.route("/")
 def index():
@@ -10,5 +24,15 @@ def index():
     return render_template('index.html', time=t)
 
 
+@app.route("/update", methods=['POST'])
+def update():
+    global UserInput
+    UserInput = request.form
+    print(UserInput)
+
+    t = datetime.now()
+    return jsonify({ 'result' : 'success', 'time' : t })
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
