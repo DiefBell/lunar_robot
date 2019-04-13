65bd782a3704ddf060cc49c3051567b50163d5e5
MerrickKing <merrickkainking@gmail.com>
2019-04-13 14:14:32 +0100

    Added support for control with a gamepad, as well as toggling the drill on and off with the START button.

--- Pi/Main.py
+++ Pi/Main.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 #import socket
-from smbus2 import SMBusWrapper 
+from smbus2 import SMBusWrapper
 
 
 addrArduino = 0x60
@@ -10,17 +10,34 @@ app = Flask(__name__)
 #host = socket.gethostbyname(socket.gethostname())
 host = '192.168.0.68'  # need to get this automagically
 
-data = []
+sensors = []
+drill = 0;
 
-InputMap =\
-{
-    "forward" : "38",
-    "back" : "40",
-    "left" : "37",
-    "right" : "39"
-}
+SPEED = 5
+
 UserInput =\
 {
+    "FACE_1" : 0,
+    "FACE_2" : 0,
+    "FACE_3" : 0,
+    "FACE_4" : 0,
+    "LEFT_TOP_SHOULDER" : 0,
+    "RIGHT_TOP_SHOULDER" : 0,
+    "LEFT_BOTTOM_SHOULDER" : 0,
+    "RIGHT_BOTTOM_SHOULDER" : 0,
+    "SELECT_BACK" : 0,
+    "START_FORWARD" : 0,
+    "LEFT_STICK" : 0,
+    "RIGHT_STICK" : 0,
+    "DPAD_UP" : 0,
+    "DPAD_DOWN" : 0,
+    "DPAD_LEFT" : 0,
+    "DPAD_RIGHT" : 0,
+    "HOME" : 0,
+    "LEFT_STICK_X" : 0,
+    "LEFT_STICK_Y" : 0,
+    "RIGHT_STICK_X" : 0,
+    "RIGHT_STICK_Y" : 0
 }
 
 @app.route("/")
@@ -32,26 +49,35 @@ def index():
 @app.route("/update", methods=['POST'])
 def update():
     global UserInput
-    global data
-    UserInput = request.form.to_dict()
+    global sensors
+    global drill
+    global SPEED
+
+    gamepad = request.form.to_dict()
+    #print(gamepad)
+
+    if gamepad["START_FORWARD"] == 1 and UserInput["START_FORWARD"] == 0:  # when the start button is pressed, but ignores holds
+        if drill == 0: drill = 1
+        else: drill = 0
+    for k, v in gamepad.items():
+        if float(v) > 0: UserInput[k] = 1
+        elif float(v) < 0: UserInput[k] = -1
+        else: UserInput[k] = 0
     print(UserInput)
 
-    userInput = UserInput.keys()
-    
-    if InputMap["forward"] in userInput:
-        data = [15, 1, 2, 3]
-    elif InputMap["back"] in userInput:
-        data = [-15, 1, 2, 3]
-    else:
-        data = [0, 1, 2, 3]
+    cmd = [0, 0, 0, 0]
+    if UserInput["DPAD_UP"] == 1 or UserInput["LEFT_STICK_Y"] == 1: cmd = [SPEED, SPEED, 0, 0]
+    elif UserInput["DPAD_DOWN"] == 1 or UserInput["LEFT_STICK_Y"] == 2: cmd = [-SPEED, -SPEED, 0, 0]
+    elif UserInput["DPAD_RIGHT"] == 1 or UserInput["LEFT_STICK_X"] == 1: cmd = [SPEED, -SPEED, 0, 0]
+    elif UserInput["DPAD_LEFT"] == 1 or UserInput["LEFT_STICK_X"] == 2: cmd = [-SPEED, SPEED, 0, 0]
     
     with SMBusWrapper(1) as bus:
-        bus.write_i2c_block_data(addrArduino, 0, data)
-    
+        bus.write_i2c_block_data(addrArduino, drill, cmd)
+
     t = datetime.now()
     return jsonify({ 'result' : 'success', 'time' : t })
 
 
 #if __name__ == "__main__":
 print("Host: %s" % host)
-app.run(host=host, port=5000, debug=False)
+app.run(host="192.168.0.50", port=5000, debug=True)
--- Pi/static/scripts.js
+++ Pi/static/scripts.js
@@ -1,3 +1,81 @@
+var updateInterval = 100;
+
+var userInput = {
+    FACE_1: 0,
+    FACE_2: 0,
+    FACE_3: 0,
+    FACE_4: 0,
+    LEFT_TOP_SHOULDER: 0,
+    RIGHT_TOP_SHOULDER: 0,
+    LEFT_BOTTOM_SHOULDER: 0,
+    RIGHT_BOTTOM_SHOULDER: 0,
+    SELECT_BACK: 0,
+    START_FORWARD: 0,
+    LEFT_STICK: 0,
+    RIGHT_STICK: 0,
+    DPAD_UP: 0,
+    DPAD_DOWN: 0,
+    DPAD_LEFT: 0,
+    DPAD_RIGHT: 0,
+    HOME: 0,
+    LEFT_STICK_X: 0,
+    LEFT_STICK_Y: 0,
+    RIGHT_STICK_X: 0,
+    RIGHT_STICK_Y: 0
+}
+
+$(document).ready(function(){
+    window.gamepad = new Gamepad();
+    window.gamepad.bind(Gamepad.Event.CONNECTED, function(device){
+        console.log('Connected', device);
+    });
+    window.gamepad.bind(Gamepad.Event.DISCONNECTED, function(device){
+        console.log('Disconnected', device);
+    });
+    window.gamepad.bind(Gamepad.Event.TICK, function(gamepads){
+        var gamepad,
+			wrap,
+			control,
+			value,
+			i,
+			j;
+
+        for(i = 0; i < gamepads.length; i++)
+        {
+            gamepad = gamepads[i];
+            if(gamepad)
+            {
+                for (control in gamepad.state)
+                {
+                    userInput[control] = gamepad.state[control];
+                    //console.log(gamepad.state[control]);
+                }
+            }
+        }
+    });
+    if(!window.gamepad.init() )
+    {
+        console.log("Gamepads not supported by current browser...");
+    }
+
+    setTimeout(update, updateInterval)
+});
+
+function update(){
+    $.ajax({
+        url: "/update",
+        type: "post",
+        data: userInput,
+        success: function(data){
+            $("#time").html(data.time);
+        },
+        complete: function() {
+            setTimeout(update, updateInterval);
+        }
+    });
+}
+
+/*
 $(document).ready(function() {
     console.log("READY  BOIS");
     setTimeout(update, 200);
@@ -36,4 +114,4 @@ function update() {
             setTimeout(update, 200);
         }
 });
-}
+}*/
