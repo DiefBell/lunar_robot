874f61f2fc4d36050cce4ee3ec4accac992bea58
MerrickKing <diefenbaker.bell@gmail.com>
2019-05-02 10:42:55 +0100

    Pi should now send its IP to the Arduino over I2C

--- Pi/Services/PullAndSetup.py
+++ Pi/Services/PullAndSetup.py
@@ -1,10 +1,25 @@
 #!/usr/bin/python3
 
-from subprocess import Popen
+from subprocess import getoutput
+import os
 
-wkdir = "/home/pi/lunar_robot/Pi/"
-gitdir="/home/pi/lunar_robot/"
+os.chdir("/home/pi/lunar_robot/Pi/")
+pipDL = getoutput("sudo pip3 install smbus2")
+print(pipDL)
 
-Popen(["sudo", "pip3", "install", "smbus2"], cwd=wkdir)
-Popen(["sudo", "git", "reset", "--HARD"],  cwd=gitdir)
-Popen(["sudo", "git", "pull"], cwd=gitdir)
+os.chdir("/home/pi/lunar_robot/")
+gitReset = getoutput("sudo git reset --HARD")
+print(gitReset)
+gitPull = getoutput("sudo git pull")
+print(gitPull)
+
+hostname = getoutput("hostname -I")
+print(hostname)
+ip = hostname.split(' ')[0].split('.')
+print(ip)
+
+addrArduino = 0x60
+from smbus2 import SMBusWrapper
+with SMBusWrapper(1) as bus:
+	cmd = [ int(ip[0]), int(ip[1]), int(ip[2]), int(ip[3]) ]
+	bus.write_i2c_block_data(addrArduino, 3, cmd)
